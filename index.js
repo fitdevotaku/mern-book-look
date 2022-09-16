872bb74b93622ba1255d25f2905584be9dfa688a
sm3 <devthree@scipiomundine.com>
2022-09-16 00:22:35 -0700

    edit changes to database console

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB connection successful!"))
+  .then(() => console.log("MongoDB connection is successful!"))
   .catch((err) => {
     console.log(err);
   });
