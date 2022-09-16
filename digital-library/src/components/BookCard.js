f148baa4b6e2b6bfaf643b2b3fb04ee358db5245
sm3 <devthree@scipiomundine.com>
2022-09-16 02:26:11 -0700

    component updates

---
+++ digital-library/src/components/BookCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const BookCard = (props) => {
+  const book = props.book;
+
+  return (
+    <div className="card-container">
+      <img
+        src="https://www.infobooks.org/free-pdf-books/sports/bodybuilding/"
+        alt=""
+      />
+      <div className="details">
+        <h2>
+          <Link to={`/display-book/${book._id}`}>{book.title}</Link>
+        </h2>
+        <h3>{book.author}</h3>
+        <p>{book.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
