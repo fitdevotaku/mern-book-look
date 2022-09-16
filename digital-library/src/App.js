319b1946adf2ad12ff563544bb93b03ca83ac28e
sm3 <devthree@scipiomundine.com>
2022-09-16 02:06:20 -0700

    updated

--- digital-library/src/App.js
+++ digital-library/src/App.js
@@ -1,23 +1,25 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NewBook from "./components/NewBook";
+import BookList from "./components/BookList";
+import BookDetails from "./components/BookDetails";
+import BookUpdate from "./components/BookUpdate";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={BookList} />
+          <Route path="/create-book" component={NewBook} />
+          <Route path="/book-edit/:id" component={BookUpdate} />
+          <Route path="/display-book/:id" component={BookDetails} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
